f70a29fd9b543ff7ae2a0c7ac44eb727299e9c08
sander1 <sander@plexapp.com>
2012-02-12 13:11:15 +0100

    Cleanup bundle file

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,75 +1,66 @@
-VIDEO_PREFIX = "/video/wimp"
-
-NAME = L('Title')
-
+NAME = 'Wimp'
 ART  = 'art-default.jpg'
 ICON = 'icon-default.png'
-
 WIMP_URL = 'http://wimp.com'
-####################################################################################################
 
+####################################################################################################
 def Start():
 
-    Plugin.AddPrefixHandler(VIDEO_PREFIX, VideoMainMenu, NAME, ICON, ART)
+    Plugin.AddPrefixHandler("/video/wimp", VideoMainMenu, NAME, ICON, ART)
 
     Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
     Plugin.AddViewGroup("List", viewMode="List", mediaType="items")
-    
+
     ObjectContainer.title1 = NAME
     ObjectContainer.view_group = "List"
     ObjectContainer.art = R(ART)
     DirectoryObject.thumb = R(ICON)
-    VideoItem.thumb = R(ICON)
-    
+    VideoClipObject.thumb = R(ICON)
+
     HTTP.CacheTime = CACHE_1HOUR
 
- 
-
+####################################################################################################
 def VideoMainMenu():
 
     oc = ObjectContainer(view_group="InfoList")
     oc.add(DirectoryObject(key=Callback(NewestVideos, title="Newest Videos"), title="Newest Videos", summary="Most Recent Videos uploaded on Wimp.com"))
     oc.add(DirectoryObject(key=Callback(OlderVideos, title="Older Videos"), title="Older Videos", summary="Videos previously uploaded on Wimp.com"))
-    oc.add(VideoClipObject(url=(WIMP_URL+'/random/'), title="Random Video", summary="Play a random Wimp.com video", thumb=R(ICON)))
+    oc.add(VideoClipObject(url=(WIMP_URL+'/random/'), title="Random Video", summary="Play a random Wimp.com video"))
 
     return oc
 
+####################################################################################################
 def NewestVideos(title):
-    
+
     oc = ObjectContainer(title1=title, view_group="InfoList")
     data = HTML.ElementFromURL(WIMP_URL)
-    
+
     for video in data.xpath('//span[@class="video_date"]'):
-        #Log(video.text)
         theDate = Datetime.ParseDate(video.text)
         delta = Datetime.Now() - theDate
         if delta.days <= 1:
             title = video.xpath('./following-sibling::a')[0].text
-            #Log(title)
             url = video.xpath('./following-sibling::a')[0].get('href')
-            #Log(url)
             oc.add(VideoClipObject(url=(WIMP_URL + url), title=title, thumb=R(ICON)))
         else:
             pass
-    
+
     return oc  
 
+####################################################################################################
 def OlderVideos(title):
-    
+
     oc = ObjectContainer(title1=title, view_group="InfoList")
     data = HTML.ElementFromURL(WIMP_URL)
-    
+
     for video in data.xpath('//span[@class="video_date"]'):
-        #Log(video.text)
         theDate = Datetime.ParseDate(video.text)
         delta = Datetime.Now() - theDate
         if delta.days > 1:
             title = video.xpath('./following-sibling::a')[0].text
-            #Log(title)
             url = video.xpath('./following-sibling::a')[0].get('href')
-            #Log(url)
             oc.add(VideoClipObject(url=(WIMP_URL + url), title=title, thumb=R(ICON)))
         else:
             pass
-    
+
     return oc
